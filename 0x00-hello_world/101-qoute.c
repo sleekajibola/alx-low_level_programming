4bb6777e9d679c129064ebdd28de63505c4268da
sleekajibola <sesanajibola20@gmail.com>
2022-06-30 17:14:38 -0700

    My qoue task

--- 0x00-hello_world/101-qoute.c
+++ 0x00-hello_world/101-qoute.c
@@ -4,12 +4,10 @@
 /**
  * main - Entry quote
  *
- * Description: prints Dora's quote
- *
- * Return: 1 (Sucess)
+ * Return: 0 (Sucess)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
