20d25138afabb7b8b0c7eeebb2f0beb0b8c69f9d
sleekajibola <sesanajibola20@gmail.com>
2022-07-02 06:44:59 -0700

    Betty be a good girl

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alway 0 (Success)
+ */
+int main(void)
+{
+	char e;
+
+	for (e = '0'; e <= '9'; e++)
+	{
+		putchar(e);
+	}
+	putchar('\n');
+	return (0);
+}
