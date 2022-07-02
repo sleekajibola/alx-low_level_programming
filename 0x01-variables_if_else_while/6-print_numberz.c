7946a3af14cf178e15103e6c368d877c6d9be22e
sleekajibola <sesanajibola20@gmail.com>
2022-07-02 06:57:23 -0700

    Betty butter

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alway 0 (Success)
+ */
+int main(void)
+{
+	int f;
+
+	for (f = '0'; f <= '9'; f++)
+	{
+		putchar(f);
+	}
+	putchar('\n');
+	return (0);
+}
