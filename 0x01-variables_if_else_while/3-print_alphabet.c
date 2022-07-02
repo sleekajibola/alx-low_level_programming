28f19e63bfb6fc4268f8e4aa5b44b830b6295a84
sleekajibola <sesanajibola20@gmail.com>
2022-07-02 05:20:47 -0700

    My betty alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *  main - Entry point
+ *
+ *  Return: Alway 0 (Success)
+ */
+int main(void)
+{
+	char d;
+
+	for (d = 'a'; d <= 'z'; d++)
+	{
+		putchar(d);
+	}
+	for (d = 'A'; d <='Z'; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
