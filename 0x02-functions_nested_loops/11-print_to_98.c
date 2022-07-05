a44f24abdc7cda4572cf9e3c3d2b4e844eae1e0a
sleekajibola <sesanajibola20@gmail.com>
2022-07-05 16:42:14 -0700

    My 98 variable

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * print_to_98 - print all natural number to 98
+ * @n: the given number
+ *
+ * Return: no return
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("%d\n", n);
+}
