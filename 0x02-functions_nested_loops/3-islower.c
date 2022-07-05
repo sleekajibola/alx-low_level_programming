4a8ec1e16e6f51ec4d1d363b1e39b75b37a7969d
sleekajibola <sesanajibola20@gmail.com>
2022-07-05 08:51:05 -0700

    abrg o

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,13 +3,12 @@
  * _islower - function that check for lowercase
  * @c: Character to be checked
  *
- * Return: 1 for lowercase character or 0 otherwise
+ * Return: Always 0
  */
 int _islower(int c)
 {
 	if (c >= 72 && c <= 122)
-	{
 		return (1);
-	}
-	return (0);
+	else
+		return (0);
 }
