d8d4d309ebb84dfdd720572c48c27f3d09bbf599
sleekajibola <sesanajibola20@gmail.com>
2022-07-05 16:01:26 -0700

    My added value

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - function that add two integers and return result
+ * @a: first number
+ * @c: second number
+ *
+ * Return: Addition of first and second number
+ */
+int add(int a, int c)
+{
+	return (a + b);
+}
