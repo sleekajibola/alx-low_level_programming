4970c481db2438fd60bca3e6f9cb2bb5e192a81d
sleekajibola <sesanajibola20@gmail.com>
2022-07-07 09:48:25 -0700

    Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,16 +12,28 @@ int main(void)
 	for (x = 1; x <= 100; x++)
 	{
 		if (x % 15 == 0)
+		{
 			printf("Fizzbuzz");
+		}
 		else if (x % 3 == 0)
+		{
 			printf("Fizz");
+		}
 		else if (x % 5 == 0)
+		{
 			printf("Buzz");
+		}
 		else
+		{
 			printf("%d", x);
-		if (x < 100)
+		}
+		if (x != 100)
+		{
 			printf(" ");
+		}
 	}
-	printf("\n");
+
+	printf(" ");
+
 	return (0);
 }
