e66e787187ecec49db8b3146422ae33a9347a3f7
sleekajibola <sesanajibola20@gmail.com>
2022-07-07 05:52:07 -0700

    My printed numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Functions that print number from 0 to 9
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	char b = 0
+
+		for (b = '0'; b <= '9'; b++)
+		{
+			_putchar(b);
+		}
+	_putchar('\n');
+}
