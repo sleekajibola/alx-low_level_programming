0dbda2f3203fda89a66725617ffc5a668217c562
sleekajibola <sesanajibola20@gmail.com>
2022-07-07 06:00:11 -0700

    My stubborn numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,11 @@
  */
 void print_numbers(void)
 {
-	char b = 0
+	char b;
 
-		for (b = '0'; b <= '9'; b++)
-		{
-			_putchar(b);
-		}
+	for (b = '0'; b <= '9'; b++)
+	{
+		_putchar(b);
+	}
 	_putchar('\n');
 }
