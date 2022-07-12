f034cc1814f8b855905ccafa92fbc46bdd80b53d
sleekajibola <sesanajibola20@gmail.com>
2022-07-12 10:24:41 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#incude "main.h"
+/**
+ * print_rev - function that print string in reverse
+ * @s: pointer in reverse
+ *
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	while (i > 0)
+	{
+		i--;
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
