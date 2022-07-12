491c0068cb424c7fd6630a94cbd188c6ff19f65e
sleekajibola <sesanajibola20@gmail.com>
2022-07-12 11:20:50 -0700

    second rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * rev_string - function that reverse a string
+ * @s: pointer to be reversed
+ *
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+
+	int length = 0, j, i;
+	char v1, v2;
+
+	while (length >= 0)
+	{
+		if (s[length] == '\0')
+			break;
+		length++;
+	}
+
+	j = lenght - 1;
+	i = 0;
+
+	while (j > i)
+	{
+		v1 = s[i];
+		v2 = s[j];
+		s[i] = v2;
+		s[j] = v1;
+		j--;
+		i++;
+	}
+}
