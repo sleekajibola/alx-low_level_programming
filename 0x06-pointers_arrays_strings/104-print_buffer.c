987adcb7ae1ac455326709f13757057a24a405c6
sleekajibola <sesanajibola20@gmail.com>
2022-07-14 17:01:01 -0700

    hmmmm

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -3,58 +3,48 @@
 
 /**
  * print_buffer - prints a buffer
- * @c: buffer
- * @s: bytes of buffer
- * @l: line of buffer
+ * @b: buffer to be printed
+ * @size: number of bytes to be printed
  *
  * Return: void
  */
 void print_buffer(char *b, int size)
 {
-	int j, k;
+	int bytes, index;
 
-	for (j = 0; j <= 9; j++)
+	for (byte = 0; byte < size; byte += 10)
 	{
-		if (j <= s)
-			printf("%02x", c[l * 10 + j]);
-		else
-			printf(" ");
-		if (j % 2)
-			putchar(' ');
-	}
-	for (k = 0; k <= s; k++)
-	{
-		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-			putchar(c[l * 10 + k]);
-		else
-			putchar('.');
-	}
-}
+		printf("%08x: ", byte);
 
-/**
- * print_buffer - prints a buffer
- * @b: buffer
- * @size: size of buffer
- *
- * Return: void
- */
-void print_buffer(char *b, int size)
-{
-	int i;
-
-	for (i = 0; i <= (size - 1) / 10 && size; i++)
-	{
-		printf("%08x: ", i * 10);
-		if (i < size / 10)
+		for (index = 0; index < 10; index++)
 		{
-			print_line(b, 9, i);
+			if ((index + byte) >= size)
+				printf("  ");
+
+			else
+				printf("%02x", *(b + index + byte));
+
+			if ((index % 2) != 0 && index != 0)
+				printf(" ");
 		}
-		else
+
+		for (index = 0; index < 10; index++)
 		{
-			print_line(b, size % 10 - 1, i);
+			if ((index + byte) >= size)
+				break;
+
+			else if (*(b + index + byte) >= 31 && *(b + index + byte) <= 126)
+				printf("%c", *(b + index + byte));
+
+			else
+				printf(".");
 		}
-		putchar('\n');
+
+		if (byte >= size)
+			continue;
+
+		printf("\n");
 	}
-	if (size == 0)
-		putchar('\n');
+	if (size <= 0)
+		printf("\n");
 }
