3b8a7e31bac8fe9ae17b0fa3d2c68bb44dcefaa0
sleekajibola <sesanajibola20@gmail.com>
2022-07-20 07:41:29 -0700

    my reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,16 +7,13 @@
  */
 void _print_rev_recursion(char *s)
 {
-	int i = 0;
+	if (s[i] != 0)
+	{
+		return;
+	}
 
-	while (s[i] != 0)
-	{
-		i++;
-		_print_rev_recursion(s[i]);
-	}
-	while (i > 0)
-	{
-		i--;
-		_putchar(s[i]);
-	}
+		s++;
+		_print_rev_recursion(s);
+		s--;
+		_putchar(*s);
 }
