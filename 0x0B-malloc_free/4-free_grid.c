66657a6213320d94614aa9b62145f0e18d575c33
sleekajibola <sesanajibola20@gmail.com>
2022-07-25 19:25:34 -0700

    lock

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,11 +8,10 @@
  */
 void free_grid(int **grid, int height)
 {
-	int i;
-
-	if (grid == NULL || height <= 0)
-		return;
-	for (i = 0; i < height; ++i)
-		free(grid[i]);
-	free(grid);
+	if (grid != NULL && height != 0)
+	{
+		for (; height >= 0; height--)
+			free(grid[height]);
+		free(grid);
+	}
 }
